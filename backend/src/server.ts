76b285804b349bb5f71b4593cafa8c94589fd856
Pranav-Reddy29 <bunnygamingff29@gmail.com>
2026-06-22 16:00:51 +0530

    Day 2: Backend setup with Express, PostgreSQL and Prisma

---
+++ backend/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import healthRoutes from "./routes/health.routes";
+
+dotenv.config();
+
+const app = express();
+app.use("/api/health", healthRoutes);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "SmartERP Backend Running"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
